12c2f63807eaf1755bd69422cf527edbc5ee133b
forcerion <forcerion@gmail.com>
2025-02-10 14:34:34 +0100

    update

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Sélectionner tous les éléments game-screen-grid__cell
+// Ajouter un event listener sur chacun des éléments
+// Récupérer le data-x et data-y avec un console.log
+// Vider la grille et placer les comptes
+// Alterner la couleur
+
